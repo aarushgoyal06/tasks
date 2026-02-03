5cceacb2418bbe78cbfd22c93e219aff5d839ff1
aarushgoyal06 <aarushg@udel.edu>
2026-02-03 18:30:35 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
+                Aarush Goyal
+            </p>
+            <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
