8f259dc1b659f79cc007d9231441d44bdbba4008
Aarush Goyal <aarushg@udel.edu>
2026-02-19 17:58:06 -0500

    first 3 functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,11 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    } else {
+        return [numbers[0], numbers[numbers.length - 1]];
+    }
 }
 
 /**
@@ -13,7 +17,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    let newNums = numbers.map((num: number): number => num * 3);
+    return newNums;
 }
 
 /**
@@ -21,7 +26,8 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    let newStrings = numbers.map((str: string): number => parseInt(str) || 0);
+    return newStrings;
 }
 
 /**
